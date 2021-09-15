1c98af411d747456784d10bcc4df8fcac83acff2
Archan.v <archanvalvi@gmail.com>
2021-09-15 15:18:20 +0530

    firstCommit

---
+++ MavenProject1/src/main/java/Com/Lti/MavenProject1/App.java
@@ -0,0 +1,13 @@
+package Com.Lti.MavenProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
